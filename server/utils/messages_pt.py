860c73c4a97e23bc53298ada3f8d6743d7bd45cf
Joismar Braga <joismar@outlook.com>
2023-05-02 20:47:36 -0300

    feat: cria mensagens

---
+++ server/utils/messages_pt.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Message(str, Enum):
+    CHAINED = "Você formou uma CORRENTE com {} pontos!"
+    POINTS = "Você ganhou {} pontos!"
+
+class Error(str, Enum):
+    INEXISTENT_SESSION = "Sessão não existe."
+    INEXISTENT_PLAYER = "Jogador não existe."
+    ALL_PLAYERS_NEED_READY = "Todos os jogadores precisam estar prontos."
+    INVALID_TURN = "Turno de jogador inválido."
+    INEXISTENT_WORD = "Palavra não existe."
+    WORD_ALREADY_IN_GAME = "Palavra já foi usada no jogo."
+    NO_POINTS = "A palavra não formou pontos."
+    
