88c414f274dd86e3d69022ced774cc5d761dec24
Joismar Braga <joismar@outlook.com>
2023-05-06 18:11:34 -0300

    feat: implementado session repository com dynamo db

--- server/repository/session_repository.py
+++ server/repository/session_repository.py
@@ -1,30 +1,63 @@
 import json
 from entities.Player import Player
-from entities.Session import Session
+from entities.Session import Session, GameStatus
 from interfaces.i_session_repository import ISessionRepository
+from boto3.dynamodb.conditions import Key
+import boto3
 
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('sessions')
 
 class SessionRepository(ISessionRepository):
-  def get_session(self, game_name: str) -> Session:
-    with open('sessions.json', 'r', encoding='utf-8') as file:
-      file_string = file.read()
-      if (len(file_string)):
-        sessions = json.loads(file_string)
-        session_dict = sessions[game_name]
-        session_dict_players = session_dict.pop('players')
-        players = [Player(**player) for player in session_dict_players]
-
-        return Session(**session_dict, players=players)
+  def get(self, game_name: str) -> Session:
+    response = table.get_item(Key={'name': game_name})
+    item = response.get('Item')
+    if item:
+        session = Session(
+            name=item['name'],
+            _id=item['id'],
+            players=item['players'],
+            turn_index=item['turn_index'],
+            chain=item['chain'],
+            status=GameStatus(item['status'])
+        )
+        return session
   
-  def save_session(self, session: Session):
-      session_json = session.to_dict()
-      
-      with open('./sessions.json', 'w+', encoding='utf-8') as file:
-        sessions = {}
-        file_string = file.read()
-        if (len(file_string)):
-          sessions = json.loads(file_string)
-        
-        sessions[session.name] = session_json
-        file.write(json.dumps(sessions, indent=2))
-        return sessions[session.name]
+  def save(self, session: Session):
+    item = {
+      'name': session.name,
+      'id': session.id,
+      'players': [player.to_dict() for player in session.players],
+      'turn_index': session.turn_index,
+      'chain': session.chain,
+      'status': session.status.value,
+    }
+    
+    table.put_item(Item=item)
+    return session
+    
+  def delete(self, session_id):
+    table.delete_item(Key={'id': session_id})
+    
+  def get_by_name(self, session_name: str):
+    print(session_name)
+    response = table.query(
+      IndexName='name-index',
+      KeyConditionExpression=Key('name').eq(session_name)
+    )
+    print(response)
+    items = response.get('Items')
+    if items:
+      session_data = items[0]
+      players = session_data.get('players', [])
+      for player in players: player['_id'] = player.pop('id')
+      session = Session(
+        _id=session_data['id'],
+        name=session_data['name'],
+        players=[Player(**player) for player in players],
+        turn_index=session_data.get('turn_index', 0),
+        chain=session_data.get('chain', []),
+        status=GameStatus(session_data.get('status', 'CREATED'))
+      )
+      return session
