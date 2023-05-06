1a24ec8128f0e85c133a58329fd1260ffce398b9
Joismar Braga <joismar@outlook.com>
2023-05-06 18:11:59 -0300

    refactor: refatorado main e game

--- server/game.py
+++ server/game.py
@@ -4,60 +4,65 @@ from entities.Session import GameStatus, Session
 from interfaces.i_session_repository import ISessionRepository
 from utils.messages_pt import Error, Message
 from utils.calculate_score import calculate_score
+from utils.action_response import ActionResponse
 
 
 class Game:
   def __init__(self, session_repository: ISessionRepository) -> None:  
     self.session_repository: ISessionRepository = session_repository
     self.words = self.load_words()
+    self.connection_id = None
 
   def load_words(self):
-    with open('server/wordlist.txt', 'r', encoding='utf-8') as f:
+    with open('wordlist.txt', 'r', encoding='utf-8') as f:
       return f.read().split('\n')
+      
+  def set_connection_id(self, connection_id: str):
+    self.connection_id = connection_id
     
   def host(self, player_name: str):
-    session: Session = Session("_".join(random.sample(self.words, 2)))
-    player: Player = Player(player_name)
+    session: Session = Session("_".join(random.sample(self.words, 2)), _id=self.connection_id)
+    player: Player = Player(player_name, _id=self.connection_id)
     session.players.append(player)
-    self.session_repository.save_session(session)
+    session.save()
     
-    return {"player_id": player.id, "session_id": session.id, "game_name": session.name, "game_status": int(session.status)}
+    return ActionResponse('host', {"player_id": player.id, "session_id": session.id, "game_name": session.name, "game_status": int(session.status)})
 
   def join(self, game_name: str, player_name: str):
-    session: Session = self.session_repository.get_session(game_name)
+    session: Session = self.session_repository.get_by_name(game_name)
     if not session: return self.__error(Error.INEXISTENT_SESSION)
-    player: Player = Player(player_name)
+    player: Player = Player(player_name, _id=self.connection_id)
     session.players.append(player)
-    self.session_repository.save_session(session)
+    session.save()
 
-    return {"player_id": player.id, "session_id": session.id, "game_name": session.name, "game_status": int(session.status)}
+    return ActionResponse('join', {"player_id": player.id, "session_id": session.id, "game_name": session.name, "game_status": int(session.status)})
   
-  def condition(self, player_id: str, player_condition: PlayerCondition, game_name: str):
-    session: Session = self.session_repository.get_session(game_name)
+  def condition(self, player_id: str, player_condition: PlayerCondition, session_id: str):
+    session: Session = self.session_repository.get(session)
     player: Player = session.find_player(player_id)
     if not player: return self.__error(Error.INEXISTENT_PLAYER)
     player.condition = player_condition
-    self.session_repository.save_session(session)
+    session.save()
 
-    return {"players_conditions": [{"player_id": x.id, "player_condition": x.condition} for x in session.players]}
+    return ActionResponse('condition', {"players_conditions": [{"player_id": x.id, "player_condition": x.condition} for x in session.players]})
 
-  def start(self, game_name: str):
-    session: Session = self.session_repository.get_session(game_name)
+  def start(self, session_id: str):
+    session: Session = self.session_repository.get(session_id)
     if not session: return self.__error(Error.INEXISTENT_SESSION)
     if not self.__ready_to_start(session): return self.__error(Error.ALL_PLAYERS_NEED_READY)
     session.status = GameStatus.STARTED
     session.chain.append(random.choice(self.words))
-    self.session_repository.save_session(session)
+    session.save()
   
-    return {
+    return ActionResponse('start', {
       "game_status": int(session.status), 
       "game_data": {
         "words": session.chain, 
         "players": [{"name": x.name, "player_id": x.id, "points": x.score} for x in session.players],
-        "turn": session.turn_index}}
+        "turn": session.turn_index}})
   
-  def word(self, game_name: str, player_id: str, word: str):
-    session: Session = self.session_repository.get_session(game_name)
+  def word(self, session_id: str, player_id: str, word: str):
+    session: Session = self.session_repository.get(session_id)
     if not session: return self.__error(Error.INEXISTENT_SESSION)
     if not session.turn_player.id == player_id: return self.__error(Error.INVALID_TURN)
     if word not in self.words: return self.__error(Error.INEXISTENT_WORD)
@@ -76,17 +81,27 @@ class Game:
     messages = [Message.POINTS.value.format(score)]
     if chain_score > 0: messages.append(Message.CHAINED.value.format(chain_score))
 
-    return {
+    return ActionResponse('word', {
       "messages": messages, 
       "game_data": {
         "words": session.chain, 
-        "players": [{"name": x.name, "player_id": x.id, "points": x.score} for x in session.players]}}
+        "players": [{"name": x.name, "player_id": x.id, "points": x.score} for x in session.players]}})
   
-  def word_time_init(self):
-    pass
+  def set_word_time(self, session_id: str, player_id: str, word_time: float):
+    session: Session = self.session_repository.get(session_id)
+    if not session: return self.__error(Error.INEXISTENT_SESSION)
+    player: Player = session.find_player(player_id)
+    if not player: return self.__error(Error.INEXISTENT_PLAYER)
+
+    player.last_word_time = word_time
+
+    session.save()
+    
+  def disconnect(self):
+    self.session_repository.delete(self.connection_id)
   
   def __ready_to_start(self, session: Session):
-    return all([x.condition == PlayerCondition.READY for x in session.players])
+    return all(x.condition == PlayerCondition.READY for x in session.players)
   
   def __error(self, message: Error):
     return {"error": True, "message": message.value, "code": message.name}
--- server/main.py
+++ server/main.py
@@ -1,30 +1,48 @@
+import json
+import traceback
 from repository.session_repository import SessionRepository
 from game import Game
 from utils.http_response import HttpResponse
+from entities.RepositoryManager import RepositoryManager
 
 
 def main(event, context):
-  session_respository = SessionRepository()
-  game = Game(session_respository)
+  print(json.dumps(event))
+  print(context)
+  
+  session_repository = SessionRepository()
+  RepositoryManager.set_repository(session_repository)
+  game = Game(session_repository)
 
   if 'requestContext' not in event: return HttpResponse.BadRequest('requestContext is required')
+  
+  game.set_connection_id(event['requestContext']['connectionId'])
+  
   if 'routeKey' not in event['requestContext']: return HttpResponse.BadRequest('requestContext.routeKey is required')
-  if 'body' not in event['requestContext']: return HttpResponse.BadRequest('requestContext.body is required')
-  if 'data' not in event['requestContext']['body']: return HttpResponse.BadRequest('requestContext.body.data is required')
-  if isinstance(event['requestContext']['body']['data']) != list: return HttpResponse.BadRequest('requestContext.body.data should be a list')
+  
+  if event['requestContext']['routeKey'] == '$connect': return HttpResponse.Ok('', {'data': 'Success'})
+  if event['requestContext']['routeKey'] == '$disconnect': return game.disconnect()
+  
+  if 'body' not in event: return HttpResponse.BadRequest('body is required')
+  if 'data' not in event['body']: return HttpResponse.BadRequest('body.data is required')
+  
+  body = json.loads(event['body'])
+  
+  if not isinstance(body['data'], list): return HttpResponse.BadRequest('body.data should be a list')
 
   switch = {
     'host': game.host,
     'join': game.join,
     'condition': game.condition,
     'start': game.start,
-    'word': game.word
+    'word': game.word,
   }
 
   try:
-    response = switch[event['requestContext']['routeKey']](*event['requestContext']['body']['data'])
-    return HttpResponse.Ok({'data': response})
+    response = switch[body['action']](*body['data'])
+    return HttpResponse.Ok(response.action, {'data': response.data})
   except KeyError:
     return HttpResponse.BadRequest('Invalid command!')
   except Exception as e:
+    traceback.print_exc()
     return HttpResponse.InternalServerError('Internal error!', str(e))
